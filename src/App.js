cc91890d62d1b85806b65a07e50f3f23c0385c03
NicoHalpe <69744829+NicoHalpe@users.noreply.github.com>
2022-02-01 21:04:28 -0300

    Implemented lazy loading

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import SectionMe from "./components/SectionMe";
-import SectionAbout from "./components/SectionAbout";
 import "./App.css";
-import { useEffect, useState } from "react";
-import SectionSkills from "./components/SectionSkills";
-import SectionProyects from "./components/SectionProyects";
-import SectionContact from "./components/SectionContact";
+import { useEffect, useState, lazy, Suspense } from "react";
+
+import SectionMe from "./components/SectionMe";
+//import SectionAbout from "./components/SectionAbout";
+//import SectionSkills from "./components/SectionSkills";
+//import SectionProyects from "./components/SectionProyects";
+//import SectionContact from "./components/SectionContact";
+const SectionAbout = lazy(() => import("./components/SectionAbout"));
+const SectionSkills = lazy(() => import("./components/SectionSkills"));
+const SectionProyects = lazy(() => import("./components/SectionProyects"));
+const SectionContact = lazy(() => import("./components/SectionContact"));
 
 function App() {
 	const [images, setImages] = useState();
@@ -123,10 +128,18 @@ function App() {
 	return (
 		<>
 			<SectionMe />
-			<SectionAbout />
-			<SectionSkills />
-			<SectionProyects images={images} startCard={startCard} cardWidth={cardWidth} mobileDif={mobileDif} />
-			<SectionContact />
+			<Suspense fallback={<section className="about"></section>}>
+				<SectionAbout />
+			</Suspense>
+			<Suspense fallback={<section className="skills"></section>}>
+				<SectionSkills />
+			</Suspense>
+			<Suspense fallback={<section className="proyects"></section>}>
+				<SectionProyects images={images} startCard={startCard} cardWidth={cardWidth} mobileDif={mobileDif} />
+			</Suspense>
+			<Suspense fallback={<section className="contact"></section>}>
+				<SectionContact />
+			</Suspense>
 		</>
 	);
 }
